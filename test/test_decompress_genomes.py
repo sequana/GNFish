9e2edbc70ef19e266f6dc774d969376929062913
Etienne Kornobis <ekornobis@gmail.com>
2024-02-09 15:25:20 +0100

    Adding tests for decompress_genomes

---
+++ test/test_decompress_genomes.py
@@ -0,0 +1,14 @@
+import subprocess
+import gnfish.decompress_genomes as dg
+import shutil
+from pathlib import Path
+
+
+def test_main(tmp_path):
+    # dg.main()
+    data_dir = tmp_path / "data"
+    shutil.copytree(Path(__file__).parent / "data", data_dir)
+    subprocess.call(f"decompress_genomes --directory {data_dir}", shell=True)
+
+    with open(tmp_path / "data" / "genome.fa") as f:
+        assert f.readlines() == [">1\n", "CAGTCGATGCTAGTCGTAGCTGATGTCGATGCTA\no"]
