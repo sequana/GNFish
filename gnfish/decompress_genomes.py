290a1302521392bd05cf8404c94ecab78359f9b4
Etienne Kornobis <ekornobis@gmail.com>
2024-02-09 15:25:00 +0100

    refactoring to use click

--- gnfish/decompress_genomes.py
+++ gnfish/decompress_genomes.py
@@ -6,35 +6,21 @@ Created on Tue Apr 20 18:10:29 2021
 @author: hectorlorente
 """
 import os
-import urllib
-from Bio import Entrez
-import sys
 import re
-from BioSQL import BioSeqDatabase
 import argparse
-import re
-import zipfile
-from class_list_files import list_files
+from gnfish.class_list_files import list_files
+
+import click
 
 import gzip
 import shutil
+
 ##Arguments
-parser = argparse.ArgumentParser(description='Decompress .gz files.')
-parser.add_argument('--directory', help='sets path to custom folder', type=str)
-parser.add_argument('--genomic', help='looks at ./Data/Genomic', nargs='?', const='genomic', type=str)
-parser.add_argument('--rna', help='looks at ./Data/Rna', nargs='?', const='rna', type=str)
-parser.add_argument('--protein', help='looks at ./Data/Protein', nargs='?', const='protein', type=str)
-args = parser.parse_args()
-genomic = args.genomic
-rna = args.rna
-protein = args.protein
-directory = args.directory
-path = os.getcwd()
-data_type=[genomic, rna, protein]
 
 
 def select_files(directory):
-    return list_files.list_files_method(list_files,directory)
+    return list_files.list_files_method(list_files, directory)
+
 
 def decompress_genomes(directory):
     found = False
@@ -42,21 +28,46 @@ def decompress_genomes(directory):
         # print(folder[1])
         try:
             for i in range(len(folder[1])):
-                file = re.search('(.*).gz', folder[1][i])
+                file = re.search("(.*).gz", folder[1][i])
                 if file:
-                    with gzip.open(folder[1][i], 'rb') as f_in:
-                        with open(file.group(1), 'wb') as f_out:
+                    with gzip.open(folder[1][i], "rb") as f_in:
+                        with open(file.group(1), "wb") as f_out:
                             shutil.copyfileobj(f_in, f_out)
-                            found =True
-                            print('Decompressing '+ folder[1][i] + ' file.')
+                            found = True
+                            print("Decompressing " + folder[1][i] + " file.")
         except IndexError:
             continue
     if not found:
-        print('WARNING. No file decompressed.')
+        print("WARNING. No file decompressed.")
 
-if directory is not None:
-    decompress_genomes(directory + '/*')
-else:
-    for data in data_type:
-        if data is not None:
-            decompress_genomes(path+'/../Data/'+ data.capitalize()+'/*')
+
+@click.command()
+@click.option("--directory", help="sets path to custom folder", type=str)
+@click.option("--genomic", help="looks at ./Data/Genomic", is_flag=True)
+@click.option("--rna", help="looks at ./Data/Rna", is_flag=True)
+@click.option("--protein", help="looks at ./Data/Protein", is_flag=True)
+def main(directory, genomic, rna, protein):
+    path = os.getcwd()
+    data_type = [genomic, rna, protein]
+    # Process further based on the options provided
+
+    # def main():
+    #     parser = argparse.ArgumentParser(description="Decompress .gz files.")
+    #     parser.add_argument("--directory", help="sets path to custom folder", type=str)
+    #     parser.add_argument("--genomic", help="looks at ./Data/Genomic", nargs="?", const="genomic", type=str)
+    #     parser.add_argument("--rna", help="looks at ./Data/Rna", nargs="?", const="rna", type=str)
+    #     parser.add_argument("--protein", help="looks at ./Data/Protein", nargs="?", const="protein", type=str)
+    #     args = parser.parse_args()
+    #     genomic = args.genomic
+    #     rna = args.rna
+    #     protein = args.protein
+    #     directory = args.directory
+    #     path = os.getcwd()
+    #     data_type = [genomic, rna, protein]
+
+    if directory is not None:
+        decompress_genomes(directory + "/*")
+    else:
+        for data in data_type:
+            if data is not None:
+                decompress_genomes(path + "/../Data/" + data.capitalize() + "/*")
