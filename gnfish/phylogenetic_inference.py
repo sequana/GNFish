60f9938257113770c8899b9013a7500e540fdba2
Hector Lorente <hlorente@ucm.es>
2024-02-26 10:34:00 +0100

    Phylo inference to click

--- gnfish/phylogenetic_inference.py
+++ gnfish/phylogenetic_inference.py
@@ -6,44 +6,32 @@ Created on Sat Dec 11 16:32:14 2021
 @author: hectorlorente
 """
 
-import re
-import argparse
+import click
 import subprocess
 
 
-def main():
-    # Arguments
-    parser = argparse.ArgumentParser(
-        description="Phylogenetic inference using IQ-Tree."
-    )
-    parser.add_argument("iqtree_path", help="path to IQ-TREE bin folder", type=str)
-    parser.add_argument(
-        "input_file",
-        help="alignment (FASTA file with prot or nucl aligned seqs)",
-        type=str,
-    )
-    parser.add_argument(
-        "--iqtree_parameters",
-        help="command to run IQ-Tree, without output declaration, see next parameter. Default TEST, ULTRAFAST BOOSTRAP 1000, ALRT 1000 . See IQ-Tree manual for more options.",
-        nargs="?",
-        const="-m TEST -B 1000 -alrt 1000",
-        type=str,
-        default="-m TEST -B 1000 -alrt 1000",
-    )
-    parser.add_argument(
-        "--output_suffix",
-        help="suffixes added to input file. Recommended, model and type of support. Default _TEST_UFBS_alrt",
-        nargs="?",
-        const="_TEST_UFBS_alrt",
-        type=str,
-        default="_TEST_UFBS_alrt",
-    )
-    args = parser.parse_args()
-    iqtree_path = re.sub("'", "", args.iqtree_path)
-    input_file = re.sub("'", "", args.input_file)
-    iqtree_parameters = args.iqtree_parameters
-    output_suffix = args.output_suffix
+@click.command()
+@click.argument("iqtree_path", type=click.STRING)
+@click.argument("input_file", type=click.STRING)
+@click.option(
+    "--iqtree_parameters",
+    type=click.STRING,
+    help="command to run IQ-Tree, without output declaration, see next parameter. Default TEST, ULTRAFAST BOOSTRAP 1000, ALRT 1000 . See IQ-Tree manual for more options.",
+    default="-m TEST -B 1000 -alrt 1000",
+)
+@click.option(
+    "--output_suffix",
+    type=click.STRING,
+    help="suffixes added to input file. Recommended, model and type of support. Default _TEST_UFBS_alrt",
+    default="_TEST_UFBS_alrt",
+)
+def main(iqtree_path, input_file, iqtree_parameters, output_suffix):
+    """Conduct phylogenetic inference using IQ-Tree.
 
+        IQTREE_PATH path to IQ-TREE bin folder.
+
+    INPUT_FILE alignment (FASTA file with prot or nucl aligned seqs.
+    """
     full_iqtree_command = f"{iqtree_path}iqtree2 -s {input_file} {iqtree_parameters} --prefix {input_file}{output_suffix}"
     iqtree_command = full_iqtree_command.split()
     subprocess.run(iqtree_command)
